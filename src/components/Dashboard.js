11f1966e090dc18a994e0d9c946c9dcfb4adf029
BlesMateo <blesildaangelicamateo@gmail.com>
2022-01-02 10:15:37 -0500

    Changed the name of each value key to getValue

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,27 +10,27 @@ import {
   getMostPopularDay,
   getInterviewsPerDay,
 } from "helpers/selectors";
- 
+
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6,
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm",
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday",
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3",
+    getValue: getInterviewsPerDay,
   },
 ];
 
